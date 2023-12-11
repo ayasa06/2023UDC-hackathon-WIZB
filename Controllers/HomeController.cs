7db75ca8b06ebb32ca9fa3cfe11055a382ffc4d1
ayasa06 <111953500+ayasa06@users.noreply.github.com>
2023-12-11 12:58:57 +0900

    s

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search_Results()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRoute()
+        {
+            return View();
+        }
+
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+
+        public IActionResult route_detail()
+        {
+            return View();
+        }
+        public IActionResult route()
+        {
+            return View();
+        }
+        public IActionResult acsess()
+        {
+            return View();
+        }
+        public IActionResult check()
+        {
+            return View();
+        }
+
+        public IActionResult notice()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# WIZB-UDC
